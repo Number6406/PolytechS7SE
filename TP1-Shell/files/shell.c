5c6571f818e9b1a689ac54202e18e341c3cbc3b2
Talenie <alicia.abonnenc@hotmail.com>
2016-12-04 17:24:09 +0100

    jen ai marre des pipe

--- TP1-Shell/files/shell.c
+++ TP1-Shell/files/shell.c
@@ -8,11 +8,12 @@
 #include <fcntl.h>
 #include <string.h>
 
+
 #include "readcmd.h"
 
 
 void dup_out(char* fichier){
-	int file = open(fichier, O_WRONLY | O_CREAT | O_TRUNC);
+	int file = open(fichier, O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU);
 	if(file < 0) {
 		perror("Error : Output file\n");
 		exit(2);
@@ -30,30 +31,27 @@ void dup_in(char* fichier){
 		perror("Error : No input file\n");
 		exit(2);
 	}
-	if(dup2(file,0) < 0) {
+	if(dup2(0,file) < 0) {
 		perror("Error : Input Redirection error\n");
 		exit(2);
 	}
-	close(file);
+	close(0);
 }
 
 
-int main()
-{
-	int end = 0;
-
-	while (!end) {
+int main() {
+	while (1) {
 		struct cmdline *l;
 		int i = 0;
 		int pid, fpid;
 		int exec = 0;
+		int p[2];
 
 		printf("shell> ");
 		l = readcmd();
 
 		// Terminaison du shell
 		if(strcmp(l->seq[0][0], "exit") == 0) {
-			end = 1;
 			exit(0);
 		}
 
@@ -66,56 +64,50 @@ int main()
 				/* Si il n'y a pas d'erreur de redirection d'entrée ou de sortie on execute la commande */
 				if(l->err) {
 					printf("Error : %s\n", l->err);
+					exit(1);
 				}
-				else {
-
-					if( l->out ) { // Si il y a redirection de fichier en sortie.
-						dup_out(l->out);
-					}
-					if( l->in ) { // Si il y a redirection de fichier en entrée
-						dup_in(l->in);
-					}
-
-					// Le Pipe //
-					int p[2];
-
-					int res_pipe = pipe(p);
-
-					if(res_pipe < 0) {
-						printf("Erreur à la création du pipe %d.\n", i+1);
-						exit(1);
-					}
+			
+				if( l->out ) { // Si il y a redirection de fichier en sortie.
+					dup_out(l->out);
+				}
+				if( l->in ) { // Si il y a redirection de fichier en entrée
+					dup_in(l->in);
+				}
+				
+				// Le Pipe //
+				int res_pipe = pipe(p);
+				if(res_pipe < 0) {
+					printf("Erreur à la création du pipe %d.\n", i+1);
+					exit(1);
+				}
 
 
-					for (i=0; l->seq[i]!=0; i++) {
+				for (i=0; l->seq[i]!=0; i++) {
+					fpid = fork();
+					if(fpid == 0) {
+						close(p[1]);
+						dup2(p[1], STDOUT_FILENO);
+						close(p[0]);
+						printf(" commande %d : %s\n",i,l->seq[i][0]);
+						exec = execvp(l->seq[i][0],l->seq[i]);
+						printf("exécution :)\n");
 
-
-						if((fpid = fork()) == 0) {
-							close(p[1]);
-							dup2(p[1], STDOUT_FILENO);
-							close(p[0]);
-							printf(" commande %d : %s\n",i,l->seq[i][0]);
-							exec = execvp(l->seq[i][0],l->seq[i]);
-							printf("exéction :)\n");
-
-							if(exec == -1) {/* Si l'éxécution n'a pas marché on affiche l'erreur */
-								perror("Error ");
-								exit(1);
-							}
-
-						} else {
-							close(p[0]);
-							dup2(p[0], STDIN_FILENO);
-							printf("Je suis le père de %d\n", fpid);
+						if(exec == -1) {/* Si l'éxécution n'a pas marché on affiche l'erreur */
+							perror("Error ");
+							exit(1);
 						}
-						wait(0);
-						printf("Terminaison de %d\n==========================================\n", fpid);
 
+					} else {
+						close(p[0]);
+						dup2(p[0], STDIN_FILENO);
+						printf("Je suis le père de %d\n", fpid);
 					}
+					wait(0);
+					printf("Terminaison de %d\n==========================================\n", fpid);
 
-
-					//exec = execvp(l->seq[0][0],l->seq[0]);
 				}
+				
+				//exec = execvp(l->seq[0][0],l->seq[0]);
 				exit(0);
 
 			default : // Code du père
