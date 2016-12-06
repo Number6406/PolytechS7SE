e3d8e53e09f66335cb124009123b18c1f486ddcf
Gilles B <bonhoure.gilles@gmail.com>
2016-12-06 23:31:07 +0100

    V2 fonctionnelle également (si c'est bel et bien ce qu'il faut faire :kappa:

---
+++ Tp-libre/src/jus/poc/prodcons/v2/ProdCons.java
@@ -0,0 +1,88 @@
+package jus.poc.prodcons.v2;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.Semaphore;
+import jus.poc.prodcons.Message;
+import jus.poc.prodcons.Tampon;
+import jus.poc.prodcons._Consommateur;
+import jus.poc.prodcons._Producteur;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author bonhourg
+ */
+public class ProdCons implements Tampon {
+    
+    DateTimeFormatter dateFormat;
+    private Message[] tampon;
+    
+    private static Semaphore sem_prod;
+    private static Semaphore sem_cons;
+    
+    private int tete_production;
+    private int tete_consommation;
+    private int nb_messages_tampon;
+    
+    public ProdCons(int taille_tampon) {
+        dateFormat = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
+        tampon = new Message[taille_tampon];
+        
+        sem_prod = new Semaphore(taille_tampon);
+        sem_cons = new Semaphore(0);
+        
+        tete_production = 0;
+        tete_consommation = 0;
+        nb_messages_tampon = 0;
+    }
+    
+    @Override
+    public void put(_Producteur p, Message msg) throws Exception, InterruptedException {
+        
+        sem_prod.acquire();
+        synchronized(this) {
+            System.out.println("["+ dateFormat.format(LocalDateTime.now()) +"]<PROD> " + msg.toString() + " en "+tete_production);
+            //ajout dans le buffer
+            tampon[tete_production] = msg;
+            tete_production = (tete_production+1)%taille();
+
+            nb_messages_tampon++;            
+        }
+        sem_cons.release();
+        
+    }
+
+    @Override
+    public Message get(_Consommateur c) throws Exception, InterruptedException {
+        Message m;
+            
+        sem_cons.acquire();
+        synchronized(this) {
+            m = tampon[tete_consommation];
+            System.out.println("["+ dateFormat.format(LocalDateTime.now()) +"]<CONS><"+c.identification()+"> " + m.toString());
+            tete_consommation = (tete_consommation+1)%taille();
+
+            nb_messages_tampon--;           
+        }
+        sem_prod.release();
+        
+        return m;
+    }
+
+    @Override
+    public synchronized int enAttente() {
+        return nb_messages_tampon;
+    }
+
+    @Override
+    public int taille() {
+        return this.tampon.length;
+    }
+    
+}
