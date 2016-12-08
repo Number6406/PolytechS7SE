d9c5279446ac3b41bb59dd4484291ae1c61e301c
Gilles B <bonhoure.gilles@gmail.com>
2016-12-08 19:24:51 +0100

    edit de v1 pour conrrespondance au sujet

--- Tp-libre/src/jus/poc/prodcons/v1/Producteur.java
+++ Tp-libre/src/jus/poc/prodcons/v1/Producteur.java
@@ -7,6 +7,7 @@ import jus.poc.prodcons.Acteur;
 import jus.poc.prodcons._Producteur;
 import jus.poc.prodcons.Aleatoire;
 import jus.poc.prodcons.ControlException;
+import jus.poc.prodcons.Message;
 import jus.poc.prodcons.Observateur;
 
 /*
@@ -24,7 +25,7 @@ public class Producteur extends Acteur implements _Producteur {
     private final ProdCons tampon;
     private int nb_messages;
     private final int[] tpsTraitement;
-    //private MessageX messages;
+    private Message m;
     private int numero_de_message;
     
     public Producteur(ProdCons tampon,Observateur obs, int nombreMoyenDeProduction, int deviationNombreMoyenDeProduction, int tempsMoyenProduction, int deviationTempsMoyenProduction) throws ControlException{
@@ -32,10 +33,7 @@ public class Producteur extends Acteur implements _Producteur {
         this.tampon = tampon;
         nb_messages = Aleatoire.valeur(nombreMoyenDeProduction, deviationNombreMoyenDeProduction);
         this.tpsTraitement = Aleatoire.valeurs(nb_messages, tempsMoyenProduction, deviationTempsMoyenProduction);
-        numero_de_message = 0;
-        //this.messages = new MessageX(this,numero_de_message);
-        
-        //observateur.newProducteur(this);
+        numero_de_message = 0;        
     }
     
     /**
@@ -44,12 +42,12 @@ public class Producteur extends Acteur implements _Producteur {
      * @throws Exception
      */
     public void produire() throws InterruptedException, Exception{
-        Thread.sleep(tpsTraitement[nb_messages-1]); 
-        numero_de_message++;
+        Thread.sleep(tpsTraitement[nb_messages-1]);
+        m = new MessageX(this, numero_de_message++);
     }
     
     public void deposer() throws InterruptedException, Exception{
-        tampon.put(this, new MessageX(this,numero_de_message));
+        tampon.put(this, m);
         nb_messages--;
     }
     
