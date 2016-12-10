39ef34a0b1da93d94ac74277d9473d74d73ca915
Talenie <alicia.abonnenc@hotmail.com>
2016-12-10 19:08:53 +0100

    V5 semble ok

--- Tp-libre/src/jus/poc/prodcons/v5/ProdCons.java
+++ Tp-libre/src/jus/poc/prodcons/v5/ProdCons.java
@@ -2,6 +2,9 @@ package jus.poc.prodcons.v5;
 
 import jus.poc.prodcons.v3.*;
 import java.time.format.DateTimeFormatter;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import jus.poc.prodcons.Message;
 import jus.poc.prodcons.Tampon;
 import jus.poc.prodcons._Consommateur;
@@ -24,8 +27,9 @@ public class ProdCons implements Tampon {
     DateTimeFormatter dateFormat;
     private Message[] tampon;
     
-    private static MonSemaphore sem_prod;
-    private static MonSemaphore sem_cons;
+    private final Lock lock;
+    private final Condition nonPlein; 
+    private final Condition nonVide; 
     
     private int tete_production;
     private int tete_consommation;
@@ -35,8 +39,9 @@ public class ProdCons implements Tampon {
         dateFormat = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
         tampon = new Message[taille_tampon];
         
-        sem_prod = new MonSemaphore(taille_tampon);
-        sem_cons = new MonSemaphore(0);
+        this.lock = new ReentrantLock();
+        this.nonPlein = lock.newCondition();
+        this.nonVide = lock.newCondition();
         
         tete_production = 0;
         tete_consommation = 0;
@@ -45,35 +50,40 @@ public class ProdCons implements Tampon {
     
     @Override
     public void put(_Producteur p, Message msg) throws Exception, InterruptedException {
-        
-        sem_prod.P();
-        synchronized(this) {
-            
-            Logger.getInstance().productionLogger(p, msg, tete_production);
-            
+        lock.lock();
+        try{
+            while(nb_messages_tampon == taille()){
+                nonPlein.await();
+            }
             //ajout dans le buffer
             tampon[tete_production] = msg;
             tete_production = (tete_production+1)%taille();
-            nb_messages_tampon++;            
-        }
-        sem_cons.V();
-        
+            nb_messages_tampon++; 
+            Logger.getInstance().productionLogger(p, msg, tete_production);
+            nonVide.signal();
+        } finally {
+            lock.unlock();
+        }    
     }
 
     @Override
     public Message get(_Consommateur c) throws Exception, InterruptedException {
         Message m;
-            
-        sem_cons.P();
-        synchronized(this) {
-            m = tampon[tete_consommation];
-            Logger.getInstance().consommationLogger(c, m,tete_consommation);
-            tete_consommation = (tete_consommation+1)%taille();
-
-            nb_messages_tampon--;           
-        }
-        sem_prod.V();
         
+        lock.lock();
+        try{
+            while(nb_messages_tampon==0){
+                nonVide.await();
+            }
+            // Recupérer
+            m = tampon[tete_consommation];
+            tete_consommation = (tete_consommation+1)%taille();
+            nb_messages_tampon--; 
+            Logger.getInstance().consommationLogger(c, m,tete_consommation);
+            nonPlein.signal();
+        } finally {
+            lock.unlock();
+        }        
         return m;
     }
 
