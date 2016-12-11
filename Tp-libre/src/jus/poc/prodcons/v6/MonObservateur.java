23d421376e40b79a5cef2281862e752f645c35fa
Talenie <alicia.abonnenc@hotmail.com>
2016-12-11 19:34:51 +0100

    debut de MonObservateur

---
+++ Tp-libre/src/jus/poc/prodcons/v6/MonObservateur.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jus.poc.prodcons.v6;
+
+import java.time.LocalTime;
+import java.util.Map;
+import jus.poc.prodcons.Message;
+
+/**
+ *
+ * @author alicia
+ */
+public class MonObservateur {
+    
+    private Map<Message,Integer> messages;
+    
+    public void init(){
+        
+    }
+    
+    public void deposeMessage(Producteur p, Message m){
+        
+    }
+    
+    public void retraitMessage(Consommateur c, Message m){
+        
+    }
+    
+    public void traitementMessage(Consommateur c, Message m){
+        
+    }
+}
